2aab42e661276fe1ccb986a1064868264258e02e
kBohdan <67594176+Bohdan-134@users.noreply.github.com>
2024-07-15 12:59:22 +0300

    first commit

---
+++ content/js/slider.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".owl-carousel").owlCarousel({loop:!0,margin:10,autoplay:!0,smartSpeed:1e3,autoplayTimeout:6e3,dots:!0,responsive:{0:{items:1},450:{items:1},660:{items:1},1e3:{items:1}}})}));
